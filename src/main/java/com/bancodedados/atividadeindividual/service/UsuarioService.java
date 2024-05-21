c01ce732a136d1cee681a22119783c5e5354eef4
Itor Carlos <itor51carlos@gmail.com>
2024-05-21 20:27:53 -0300

    add UsuarioService

---
+++ src/main/java/com/bancodedados/atividadeindividual/service/UsuarioService.java
@@ -0,0 +1,13 @@
+package com.bancodedados.atividadeindividual.service;
+
+import com.bancodedados.atividadeindividual.model.Usuario;
+import com.bancodedados.atividadeindividual.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+}
