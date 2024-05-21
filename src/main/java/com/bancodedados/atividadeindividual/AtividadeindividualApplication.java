3dba0199f88926c2387a02ebf1b62558053f8f36
Itor Carlos <itor51carlos@gmail.com>
2024-05-21 19:53:46 -0300

    primeiro commit

---
+++ src/main/java/com/bancodedados/atividadeindividual/AtividadeindividualApplication.java
@@ -0,0 +1,13 @@
+package com.bancodedados.atividadeindividual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeindividualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeindividualApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bancodedados/atividadeindividual/model/Usuario.java
@@ -0,0 +1,44 @@
+package com.bancodedados.atividadeindividual.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Usuario {
+    @Id
+    private String cpf;
+    private String nome;
+    private LocalDate dataAniversario;
+
+    public Usuario(String cpf, String nome, LocalDate dataAniversario) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.dataAniversario = dataAniversario;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataAniversario() {
+        return dataAniversario;
+    }
+
+    public void setDataAniversario(LocalDate dataAniversario) {
+        this.dataAniversario = dataAniversario;
+    }
+}
