4c722ca6652c6a2d56276af3f56c2119c0d69910
Itor Carlos <itor51carlos@gmail.com>
2024-05-22 10:55:29 -0300

    add UsuarioController

---
+++ src/main/java/com/bancodedados/atividadeindividual/controller/UsuarioController.java
@@ -0,0 +1,13 @@
+package com.bancodedados.atividadeindividual.controller;
+
+import com.bancodedados.atividadeindividual.model.Usuario;
+import com.bancodedados.atividadeindividual.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsuarioController {
+
+}
