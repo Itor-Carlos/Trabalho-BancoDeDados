217543b96bab80362ada31df94e21e307069a823
Itor Carlos <itor51carlos@gmail.com>
2024-05-23 15:45:31 -0300

    remove imports nao utilizados

--- src/main/java/com/bancodedados/atividadeindividual/controller/UsuarioController.java
+++ src/main/java/com/bancodedados/atividadeindividual/controller/UsuarioController.java
@@ -3,13 +3,10 @@ package com.bancodedados.atividadeindividual.controller;
 import com.bancodedados.atividadeindividual.model.Usuario;
 import com.bancodedados.atividadeindividual.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
