e5822029c91f2bedcaa9089696ac1f1e81580ba6
Itor Carlos <itor51carlos@gmail.com>
2024-05-23 16:36:11 -0300

    add primeira versão de documentação usando springdoc-openapi

--- src/main/java/com/bancodedados/atividadeindividual/controller/UsuarioController.java
+++ src/main/java/com/bancodedados/atividadeindividual/controller/UsuarioController.java
@@ -2,6 +2,11 @@ package com.bancodedados.atividadeindividual.controller;
 
 import com.bancodedados.atividadeindividual.model.Usuario;
 import com.bancodedados.atividadeindividual.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +23,23 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @GetMapping()
+    @Operation(summary = "Retorna todos os usuarios cadastrados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna todos os usuarios cadastros com sucesso",
+                    content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Usuario.class, type = "array")) }),
+    })
     public ResponseEntity<List<Usuario>> findAll(){
         return ResponseEntity.ok(usuarioService.findAll());
     }
 
     @PostMapping
+    @Operation(summary = "Cadastra um usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cadastro efetudo com sucesso",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Usuario.class)) }),
+    })
     public ResponseEntity<?> saveUsuario(@RequestBody Usuario usuario){
         ArrayList<String> errorList = new ArrayList<>();
 
@@ -40,6 +57,14 @@ public class UsuarioController {
     }
 
     @GetMapping("/{cpf}")
+    @Operation(summary = "Busca um usuário com base no CPF")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o usuario com o CPF utilizado com sucesso",
+                    content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Usuario.class)) }),
+            @ApiResponse(responseCode = "404", description = "Não foi encontrado usuário com o CPF utilizado",
+                    content = { @Content(mediaType = "application/json") }),
+    })
     public ResponseEntity<?> getUsuario(@PathVariable("cpf") String cpf) {
         if(cpf == null) ResponseEntity.badRequest().body("Cpf is a required field");
 
